c601ede188cf77b132d0d0d9884a527d2d92ea60
joan-w <wamuyujoan4@gmail.com>
2020-03-02 19:49:50 +0300

    Final edits

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ order.addEventListener("click", function(){
     alert(`Ensure you have Ksh. 100 for delivery. Thank you for placing an order at Pizza Peddlers.`)            
 
 });
+
+function submit(firstName, lastName) {
+    // var firstName = document.getElementById('fname').value;
+    // var lastName = document.getElementById('lname').value;
+
+    this.fName = firstName;
+    this.lName = lastName;
+
+    var fName = document.getElementById("fname").value;
+    var lName = document.getElementById("lname").value;
+
+    if(fName === "" || lName === "") {
+        alert("Please fill in the form correclty. Thank you.");
+    }else{
+        alert("Your message has been received. We will get back to you in a short while. Thank you.");
+    }
+}
