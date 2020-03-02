e9fec730064c070f307a4e64d89234b00ed0e7b9
joan-w <wamuyujoan4@gmail.com>
2020-03-02 14:11:00 +0300

    Adding a click event listener

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const order = document.getElementById("btn");
 order.addEventListener("click", function(){
     
-    var pizzaSize = parseInt(document.getElementById("size").value);
-    var pizzaCrust = parseInt(document.getElementById("crust").value);
-    var pizzaToppings = parseInt(document.getElementById("pizzaToppings").value);
-    var quantity = parseInt(document.getElementById("quantity").value);
+    // var pizzaSize = parseInt(document.getElementById("size").value);
+    // var pizzaCrust = parseInt(document.getElementById("crust").value);
+    // var pizzaToppings = parseInt(document.getElementById("pizzaToppings").value);
+    // var quantity = parseInt(document.getElementById("quantity").value);
         
-    var totalCost = (pizzaSize + pizzaCrust + pizzaToppings) * quantity;
+    // var totalCost = (pizzaSize + pizzaCrust + pizzaToppings) * quantity;
 
-    alert(`Your order has been received amounting to ${totalCost}`);
-    prompt(`please enterb your location.`);
-    alert(`Your order will be delivered in afew, please have some ksh. 200 for delivery.`)
+    // alert(`Your order has been received amounting to ${totalCost}`);
+    // prompt(`please enterb your location.`);
+    // alert(`Your order will be delivered in afew, please have some ksh. 200 for delivery.`)
 
-    if (delivery.checked) {
+    // if (delivery.checked) {
         
-    } else {
+    // } else {
         
-    }
-    prompt(``)
+    // }
+    // prompt(``)
 
     
 
